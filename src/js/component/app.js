c615f28b52ddb783c3d9e7ded46ec65354028d57
Pamela Contreras <pame2394@gmail.com>
2021-03-12 21:31:04 +0000

    final

---
+++ src/js/component/app.js
@@ -0,0 +1,3 @@
+.remove:hover={
+    background: palegreen;
+}
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,63 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+import React, { useState } from "react";
+import { Button } from "bootstrap";
+import { useEffect } from "react/cjs/react.production.min";
 //create your first component
 export function Home() {
+	const [lista, setLista] = useState([]);
+	const [tarea, setTarea] = useState("");
+	const [contador, setContador] = useState(0);
+	const agregar = () => {
+		if (tarea == "") {
+			alert("Digite la tarea que desea ingresar");
+		} else {
+			setLista([...lista, tarea]);
+			//alert("Su tarea ha sido ingresada");
+			setTarea("");
+			setContador(contador + 1);
+		}
+	};
+	const eliminar = i => {
+		const nuevaTarea = [...lista];
+		nuevaTarea.splice(i, 1);
+		setLista(nuevaTarea);
+		setContador(contador - 1);
+		console.log(typeof contador);
+	};
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div
+			className="text-center mt-5"
+			style={{
+				background: "#27f0e7",
+				width: "30rem",
+				border: "solid",
+				margin: "auto",
+				color: "white"
+			}}>
+			<input
+				placeholder="Ingrese su tarea"
+				onChange={e => setTarea(e.target.value)}
+				value={tarea}></input>
+			<button
+				style={{
+					background: "black",
+					color: "white"
+				}}
+				onClick={agregar}>
+				Agregar tarea
+			</button>
+			{lista.map((element, i) => {
+				return (
+					<li key={i}>
+						{element}
+						<button className="remove" onClick={() => eliminar(i)}>
+							❎
+						</button>
+					</li>
+				);
+			})}
+			<div style={{ background: "#fef99a", width: "4cm" }}>
+				{contador} <strong>Total de tareas</strong>
+			</div>
 		</div>
 	);
 }
